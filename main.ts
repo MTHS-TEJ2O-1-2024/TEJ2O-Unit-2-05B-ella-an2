978045ec8c1c1b1d2c5ad3cedc20de4d85185d4c
ella-an2 <ella.an@mths.ca>
2024-09-24 11:04:30 -0400

    Programmed it to tell me the temperature in Kalvin and changed the comments

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella
+ * Created on: Sep 2024
+ * This program tells the temperature in Kalvin
 */
 
-basic.showString('Hello, World!')
+let currentTemperature: number
+let temperatureKalvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.onButtonPressed(Button.A, function () {
+    currentTemperature = input.temperature()
+    temperatureKalvin = (currentTemperature + 273.15)
+    temperatureKalvin = Math.round(temperatureKalvin)
+
+    basic.clearScreen()
+    basic.showString('The temperature is:' + (temperatureKalvin).toString() + 'K')
+})
