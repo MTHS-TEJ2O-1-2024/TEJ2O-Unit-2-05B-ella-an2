b800809086b4c90f6b5653d4f355e02f949497b1
ella-an2 <ella.an@mths.ca>
2024-09-24 15:16:50 +0000

    Programmed to show me the temperature

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,23 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import *
+
+current_temperature: number
+temperature_kalvin: number
 
 
-display.scroll("Hello, World!")
+display.clear
+display.show(Image.HAPPY)
+sleep(1)
+
+
+while True:
+    if button_a.is_pressed():
+        current_temperature = temperature
+        temperature_kalvin = current_temperature + 273.15
+        temperature_kalvin = Math.round(temperature_kalvin)
+
+        display.clear
+        display.scroll("The temperature is:" + str(temperature_kalvin) + "K")
+
