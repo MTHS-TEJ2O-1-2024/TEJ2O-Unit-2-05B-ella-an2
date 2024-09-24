0b7fa10a3a8e50739fbe49c03d67c5e9a385b528
ella-an2 <ella.an@mths.ca>
2024-09-24 15:23:19 +0000

    changed the program

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,9 +7,6 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 from time import *
 
-current_temperature: number
-temperature_kalvin: number
-
 
 display.clear
 display.show(Image.HAPPY)
@@ -20,7 +17,7 @@ while True:
     if button_a.is_pressed():
         current_temperature = temperature
         temperature_kalvin = current_temperature + 273.15
-        temperature_kalvin = Math.round(temperature_kalvin)
+
 
         display.clear
         display.scroll("The temperature is:" + str(temperature_kalvin) + "K")
